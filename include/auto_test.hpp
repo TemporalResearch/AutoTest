70c3718cbbf1d7673e198b1279e131181cc6d9b4
Michael Lynch <lyncmi07@yahoo.co.uk>
2021-03-02 12:12:15 +0000

    [Michael] #4 add doxygen documentation

--- include/auto_test.hpp
+++ include/auto_test.hpp
@@ -46,13 +46,39 @@ struct TestSuiteInfo
     }
 };
 
+/**
+ *@defgroup test_macros Macros for adding tests
+ */
 
+/**
+ * @ingroup test_macros
+ * @def IGNORE_TEST(test_number, test_name)
+ * @brief Defines a test that should be ignored.
+ *  
+ * @param test_number Issue number of the test
+ * @param test_name Test name.
+ */
 #define IGNORE_TEST(test_number, test_name) void test_name() // test ignored
 
+/**
+ * @ingroup test_macros
+ * @def TEST(test_number, test_name)
+ * @brief Defines a test.
+ * 
+ * @param test_number Issue number of the test.
+ * @param test_name Test name.
+ */
 #define TEST(test_number, test_name) void test_name(); \
     inline static const int test_name ## _auto_adder = test_push_back(TestInfo(#test_name, test_number, &test_name)); \
     void test_name()
 
+/**
+ * @ingroup test_macros
+ * @def TEST_SUITE(suite_name)
+ * @brief Defines a test suite, containing a set of tests.
+ * 
+ * @param suite_name Test suite name.
+ */
 #define TEST_SUITE(suite_name) namespace suite_name { \
         inline static std::vector<TestInfo> _testInfo;\
         int test_push_back(TestInfo testInfo) \
@@ -64,7 +90,33 @@ struct TestSuiteInfo
     }; \
     namespace suite_name
 
+/**
+ * @defgroup assertion_macros Macros for adding assertions to be used in tests.
+ */
+
+/**
+ * @ingroup assertion_macros
+ * @def ASSERT_MATCHES(actual)(matcher)
+ * @brief Asserts that a value matches against a set of matchers.
+ * 
+ * Usage:
+ * ```
+ * ASSERT_MATCHES(actual)(matcher)
+ * ```
+ * 
+ * @param actual The value to match against. 
+ * @param matcher The matcher which defines a successful match.
+ */
 #define ASSERT_MATCHES(actual) auto_test::MatcherRunner(#actual, actual).test
+
+/**
+ * @ingroup assertion_macros
+ * @def ASSERT_EQUALS(actual, expected)
+ * @brief Asserts that two values are equal using the `==` operator.
+ * 
+ * @param actual Actual value.
+ * @param expected Expected value.
+ */
 #define ASSERT_EQUALS(actual, expected) { \
         auto expVal = expected; \
         auto actVal = actual; \
@@ -73,6 +125,13 @@ struct TestSuiteInfo
             auto_test::matchers::isEqual(#expected, expVal) \
         ); }
 
+/**
+ * @ingroup assertion_macros
+ * @def ASSERT_TRUE(actual)
+ * @brief Asserts that a value is true.
+ * 
+ * @param actual Actual value.
+ */
 #define ASSERT_TRUE(actual) { \
     bool actVal = actual; \
     auto matcher_runner = auto_test::MatcherRunner(#actual, actVal); \
@@ -80,6 +139,13 @@ struct TestSuiteInfo
         auto_test::matchers::isEqual("true", true) \
     ); }
 
+/**
+ * @ingroup assertion_macros
+ * @def ASSERT_FALSE(actual)
+ * @brief Asserts that a value is true.
+ * 
+ * @param actual Actual value.
+ */
 #define ASSERT_FALSE(actual) { \
     bool actVal = actual; \
     auto matcher_runner = auto_test::MatcherRunner(#actual, actVal); \
@@ -88,12 +154,32 @@ struct TestSuiteInfo
     ); }
 
 
+/**
+ * @brief Main namespace for the auto_test framework.
+ * 
+ */
 namespace auto_test
 {
     inline std::map<std::string, std::vector<TestInfo>*> _testSuites;
 
     int add_test_suite(const TestSuiteInfo& testSuite);
+    
+    /**
+     * @brief Run a single test suite.
+     * 
+     * @param suite_name The name of the suite to be run.
+     *
+     * @return 0 is successful.
+     * @return 1 is unsuccessful.
+     */
     int run_test_suite(const std::string& suite_name);
+    
+    /**
+     * @brief To be run in the main program to automatically fun all test suites.
+     * 
+     * @return 0 is successful.
+     * @return 1 is unsuccessful.
+     */
     int run_tests();
 }
 
