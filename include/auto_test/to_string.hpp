bc9c46c3c8f0e4cf59edee116eb1f0257d8f3de0
Michael Lynch <lyncmi07@yahoo.co.uk>
2021-03-03 20:09:21 +0000

    [Michael] #4 add to_string for std::set

--- include/auto_test/to_string.hpp
+++ include/auto_test/to_string.hpp
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <vector>
+#include <set>
+#include <algorithm>
+#include <type_traits>
 
 namespace auto_test
 {
@@ -16,6 +19,13 @@ namespace auto_test
     std::string to_string(float v);
     std::string to_string(double v);
     std::string to_string(long double v);
+    
+    template<class T>
+        requires std::is_enum_v<T>
+    std::string to_string(const T& t)
+    {
+        return std::to_string((int)t);
+    }
 
     template<class T>
     std::string to_string(const T& t)
@@ -34,6 +44,23 @@ namespace auto_test
         }
 
         str += auto_test::to_string(v_t[v_t.size() - 1]) + " ]";
+
+        return str;
+    }
+
+    template<class T>
+    std::string to_string(const std::set<T>& v_t)
+    {
+        std::string str = "{ ";
+
+        std::for_each(
+            v_t.cbegin(),
+            v_t.cend(),
+            [&str](const T& value) {
+                str += auto_test::to_string(value) + ", ";
+            });
+
+        str += "}";
 
         return str;
     }
